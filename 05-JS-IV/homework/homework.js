7c9cde4726888ecf5295aa6f036e9dd177eac9ab
Mark <marfelcomputer@gmail.com>
2022-10-09 05:52:38 -0300

    first commit

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -40,7 +40,7 @@ function eliminarPropiedad(objeto, unaPropiedad) {
 function nuevoUsuario(nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
-  // Tu código:
+  // Tu código:::::
 
 }
 
